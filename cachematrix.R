9605ddb88d7086822d8e18256cd1b6414da4d97a
robertbuhmann <89123213+robertbuhmann@users.noreply.github.com>
2022-11-20 22:21:31 +1000

    Update cachematrix.R
    Code for both functions (makeCacheMatrix & cacheSolve) has been added, alongside comments explaining the code within each function.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,68 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The 'makeCacheMatrix' function creates an R object that stores a dataframe
+##and its inverse. It does this by building a set of functions and storing
+##these functions as a list in the parent environment.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()){
+  m <- NULL
+  
+  #Initialize empty dataframe and empty vector
+  
+  set <- function(y) {
+    x <<- y
+    m <<- NULL 
+    
+    #Define set() function, takes the argument y and assigns it to the object
+    #'x' in the parent environment; takes a NULL and assigns it to the 'm'
+    # object in the parent environment (clears values for 'm' stored during a
+    #prior execution of 'cacheSolve'.
+    
+  }
+  get <- function() x
+  
+  #retrieve 'x' from parent environment and use it as the getter/accessor 
+  #for dataframe 'x'
+  
+  setsolve <- function(solve) m <<- solve
+  
+  #define the setter/mutator for 'solve'
+  
+  getsolve <- function() m
+  
+  #define the getter/accessor for 'm'
+  
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
+  
+  #Assign each function as an element within a list and return the list to the 
+  #parent environment. This allows us to use the '$' operator to access the
+  #functions by name when using the 'cacheSolve' function later.
+  
 }
 
 
-## Write a short comment describing this function
+## The 'cacheSolve' function retrieves the inverse of a dataframe stored
+##by the object 'makeCacheMatrix'
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  
+  #retrieve mean for object passed in as an argument.
+  
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+    
+    #check to see if object stored as 'm' is 'NULL', if TRUE,  it returns the
+    #cached inverse matrix
+    
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
+  
+  #if !is.null is FALSE, the input dataframe is retired, the inverse 
+  #calculated and returned to the parent environment by printing the inverse 
+  #dataframe
 }
